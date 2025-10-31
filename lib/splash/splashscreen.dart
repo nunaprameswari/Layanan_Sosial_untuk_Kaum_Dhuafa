93d7312070fcdb8418069571954053132147b550
nindiappc <nindi.nindia44@gmail.com>
2025-10-31 17:39:45 +0700

    menyeting dibagan splashscreen

--- lib/splash/splashscreen.dart
+++ lib/splash/splashscreen.dart
@@ -1,20 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:layanan_sosial_untuk_kaum_dhuafa/navigation/home.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MyHomePage()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color(0xFFEBDEBF2),
-        body: Center(
-          child: Container(
-            width: 450,
-            height: 450,
-            child: Image.asset("assets/image/splash.png"),
-          ),
+    return Scaffold(
+      backgroundColor: Color(0xFFEBDEBF2),
+      body: Center(
+        child: Container(
+          width: 450,
+          height: 450,
+          child: Image.asset("assets/image/splash.png"),
         ),
       ),
     );
